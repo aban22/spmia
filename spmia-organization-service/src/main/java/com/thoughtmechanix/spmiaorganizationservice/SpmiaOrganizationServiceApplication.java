b44ac5c4a20537b6d346c74d66baa9ac227061aa
Abel Fernandez <abelfergodevelop@gmail.com>
2019-06-27 23:56:04 +0200

    Point 4.5 Page 136

---
+++ spmia-organization-service/src/main/java/com/thoughtmechanix/spmiaorganizationservice/SpmiaOrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.spmiaorganizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmiaOrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmiaOrganizationServiceApplication.class, args);
+	}
+
+}
